ced1215eef6fcc41b967ba8a80e868fc3e27181c
Jefferson Leite Ferreira <jeehferreira444@gmail.com>
2019-11-11 17:56:04 -0300

    Parte final

---
+++ pesquisar.js
@@ -0,0 +1,10 @@
+// let inputSearch = document.querySelector('#pesquisar');
+// let botaoSearch = document.querySelector('#botaoPesquisa');
+// let miraval = document.querySelector('#miraval');
+
+
+// botaoSearch.onclick = () => {
+//     if(inputSearch.value == "miraval"){
+//         miraval.innerHTML.anchor = "iraval"
+//     }
+// }
